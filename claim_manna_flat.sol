2cfcae3f1f810e1b0e95cfe881a0ff74e989d1dd
MiKoronjoo <39623615+MiKoronjoo@users.noreply.github.com>
2021-12-27 17:24:11 +0330

    Create claim_manna_flat.sol

---
+++ claim_manna_flat.sol
@@ -0,0 +1,300 @@
+// Be name Khoda
+
+// SPDX-License-Identifier: MIT
+
+// File: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Context.sol
+
+
+// OpenZeppelin Contracts v4.4.0 (utils/Context.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+// File: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+
+
+// OpenZeppelin Contracts v4.4.0 (access/Ownable.sol)
+
+pragma solidity ^0.8.0;
+
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+// File: contracts/claim_manna.sol
+
+
+pragma solidity ^0.8.10;
+
+
+
+interface IERC20 {
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei. This is the value {ERC20} uses, unless this function is
+     * overridden;
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
+    function decimals() external view returns (uint8);
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+
+interface IBrightID {
+    event Verified(address indexed addr);
+    function verifications(address addr) external view returns (uint);
+    function history(address addr) external view returns (address);
+}
+
+
+contract ClaimManna is Ownable {
+    IBrightID public brightid;
+    IERC20 public mannaToken;
+    mapping(address => uint256) public lastClaim;
+    mapping(address => uint256) private previous;
+    uint256 public maxClaimable = 7;
+    uint256 public timePeriod = 24 * 60 * 60;
+    uint256 public claimCount = 0;
+
+    constructor(address brightidAddr, address mannaAddr) {
+        brightid = IBrightID(brightidAddr);
+        mannaToken = IERC20(mannaAddr);
+    }
+
+    function setBrightID(address addr) external onlyOwner {
+        brightid = IBrightID(addr);
+    }
+
+    function setMannaToken(address addr) external onlyOwner {
+        mannaToken = IERC20(addr);
+    }
+
+    function setMaxClaimable(uint256 maxClaimable_) external onlyOwner {
+        maxClaimable = maxClaimable_;
+    }
+
+    function setTimePriod(uint256 timePeriod_) external onlyOwner {
+        timePeriod = timePeriod_;
+    }
+
+    function approveManna(address spender, uint256 amount) external onlyOwner {
+        mannaToken.approve(spender, amount);
+    }
+
+    function isVerified(address addr) external view returns (bool) {
+        return brightid.verifications(addr) > 0;
+    }
+
+    function isRegistered(address addr) external view returns (bool) {
+        return lastClaim[addr] != 0;
+    }
+
+    function toClaim(address addr) external view returns (uint256) {
+        if (lastClaim[addr] == 0) {
+            return 0;
+        }
+        uint256 amount = (block.timestamp - lastClaim[addr]) / timePeriod;
+        if (amount > maxClaimable) {
+            amount = maxClaimable;
+        }
+        return amount;
+    }
+
+    function register() external {
+        require (brightid.verifications(msg.sender) > 0, "address is not verified");
+        require (lastClaim[msg.sender] == 0, "already registered");
+        lastClaim[msg.sender] = block.timestamp;
+    }
+
+    function claim() external {
+        require (maxClaimable != 0, "contract is stopped");
+        require (brightid.verifications(msg.sender) > 0, "address is not verified");
+        require (lastClaim[msg.sender] != 0, "not registered yet");
+        uint256 time = block.timestamp;
+        address addr = msg.sender;
+        uint256 thisCount = ++claimCount;
+        while (addr != address(0) && previous[addr] != thisCount) {
+            require (lastClaim[addr] != 0 && (time - lastClaim[addr]) > timePeriod, "no manna to claim");
+            previous[addr] = thisCount;
+            addr = brightid.history(addr);
+        }
+        uint256 amount = (time - lastClaim[msg.sender]) / timePeriod;
+        if (amount > maxClaimable) {
+            amount = maxClaimable;
+        }
+        mannaToken.transfer(msg.sender, amount * 10 ** mannaToken.decimals());
+        addr = msg.sender;
+        while (addr != address(0) && previous[addr] == thisCount) {
+            lastClaim[addr] = time;
+            previous[addr] = 0;
+            addr = brightid.history(addr);
+        }
+    }
+}
+
+// Dar panah Khoda
