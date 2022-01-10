9a46d179cc54940d801b8d20766690c34753d7d5
MiKoronjoo <39623615+MiKoronjoo@users.noreply.github.com>
2022-01-10 20:46:45 +0330

    Update claim_manna.sol

--- claim_manna.sol
+++ claim_manna.sol
@@ -14,6 +14,12 @@ interface IERC20 {
 }
 
 
+interface IManna is IERC20 {
+    function mint(address to, uint256 amount) external;
+    function burn(address from, uint256 amount) external;
+}
+
+
 interface IBrightID {
     event Verified(address indexed addr);
     function verifications(address addr) external view returns (uint);
@@ -23,16 +29,14 @@ interface IBrightID {
 
 contract ClaimManna is Ownable {
     IBrightID public brightid;
-    IERC20 public mannaToken;
+    IManna public mannaToken;
     mapping(address => uint256) public lastClaim;
-    mapping(address => uint256) private previous;
     uint256 public maxClaimable = 7;
     uint256 public timePeriod = 24 * 60 * 60;
-    uint256 public claimCount = 0;
 
     constructor(address brightidAddr, address mannaAddr) {
         brightid = IBrightID(brightidAddr);
-        mannaToken = IERC20(mannaAddr);
+        mannaToken = IManna(mannaAddr);
     }
 
     function setBrightID(address addr) external onlyOwner {
@@ -40,7 +44,7 @@ contract ClaimManna is Ownable {
     }
 
     function setMannaToken(address addr) external onlyOwner {
-        mannaToken = IERC20(addr);
+        mannaToken = IManna(addr);
     }
 
     function setMaxClaimable(uint256 maxClaimable_) external onlyOwner {
@@ -51,8 +55,8 @@ contract ClaimManna is Ownable {
         timePeriod = timePeriod_;
     }
 
-    function approveManna(address spender, uint256 amount) external onlyOwner {
-        mannaToken.approve(spender, amount);
+    function approveToken(address tokenAddr, address spender, uint256 amount) external onlyOwner {
+        IERC20(tokenAddr).approve(spender, amount);
     }
 
     function isVerified(address addr) external view returns (bool) {
@@ -64,7 +68,7 @@ contract ClaimManna is Ownable {
     }
 
     function toClaim(address addr) external view returns (uint256) {
-        if (lastClaim[addr] == 0) {
+        if (lastClaim[addr] == 0 || brightid.verifications(addr) == 0) {
             return 0;
         }
         uint256 amount = (block.timestamp - lastClaim[addr]) / timePeriod;
@@ -78,6 +82,7 @@ contract ClaimManna is Ownable {
         require (brightid.verifications(msg.sender) > 0, "address is not verified");
         require (lastClaim[msg.sender] == 0, "already registered");
         lastClaim[msg.sender] = block.timestamp;
+        lastClaim[brightid.history(msg.sender)] = 0;
     }
 
     function claim() external {
@@ -85,24 +90,12 @@ contract ClaimManna is Ownable {
         require (brightid.verifications(msg.sender) > 0, "address is not verified");
         require (lastClaim[msg.sender] != 0, "not registered yet");
         uint256 time = block.timestamp;
-        address addr = msg.sender;
-        uint256 thisCount = ++claimCount;
-        while (addr != address(0) && previous[addr] != thisCount) {
-            require (lastClaim[addr] != 0 && (time - lastClaim[addr]) > timePeriod, "no manna to claim");
-            previous[addr] = thisCount;
-            addr = brightid.history(addr);
-        }
         uint256 amount = (time - lastClaim[msg.sender]) / timePeriod;
         if (amount > maxClaimable) {
             amount = maxClaimable;
         }
-        mannaToken.transfer(msg.sender, amount * 10 ** mannaToken.decimals());
-        addr = msg.sender;
-        while (addr != address(0) && previous[addr] == thisCount) {
-            lastClaim[addr] = time;
-            previous[addr] = 0;
-            addr = brightid.history(addr);
-        }
+        mannaToken.mint(msg.sender, amount * 10 ** mannaToken.decimals());
+        lastClaim[msg.sender] = time;
     }
 }
 
